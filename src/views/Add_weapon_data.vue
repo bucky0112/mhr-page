d59d156885c2351b1090f138eacbce9a14ca807c
bucky0112 <bucky0112@gmail.com>
2021-04-03 16:42:32 +0800

    add upload image to base64

--- src/views/Add_weapon_data.vue
+++ src/views/Add_weapon_data.vue
@@ -8,7 +8,7 @@ export default {
       weaponData: {
         title: '',
         intro: '',
-        img: '',
+        icon: '',
         date: '',
         operating: {
           btn_use: '',
@@ -24,6 +24,18 @@ export default {
       this.weaponData.date = new Date().getTime();
       this.addWeapon(this.weaponData);
       this.$router.push('/');
+    },
+    uploadImage() {
+      const file = document.querySelector('input[type=file]').files[0];
+      const reader = new FileReader();
+      const vm = this;
+      reader.onload = function (e) {
+        vm.weaponData.icon = e.target.result;
+      };
+      reader.onerror = function (error) {
+        alert(error);
+      };
+      reader.readAsDataURL(file);
     },
   },
 };
@@ -48,22 +60,16 @@ export default {
             <b-form-textarea
               id="intro"
               v-model="weaponData.intro"
-              placeholder="請輸入武器簡介"
-              required>
+              placeholder="請輸入武器簡介" required>
             </b-form-textarea>
           </b-form-group>
         </b-form>
       </b-col>
       <b-col>
         <b-form>
-          <b-form-group label="武器名稱:" label-for="title">
-            <b-form-input
-              id="title"
-              v-model="weaponData.title"
-              type="text"
-              placeholder="請輸入武器名稱"
-              required
-            ></b-form-input>
+          <b-form-group label="武器 icon:" label-for="icon">
+            <input @change="uploadImage" type="file" name="photo" accept="image/*" />
+            <img :src="weaponData.icon" class="image">
           </b-form-group>
 
           <b-form-group label="武器基本操作:" label-for="btn-use">
@@ -71,8 +77,8 @@ export default {
               id="btn-use"
               v-model="weaponData.btn_use"
               placeholder="請輸入武器基本操作"
-              required>
-            </b-form-textarea>
+              required
+            ></b-form-textarea>
           </b-form-group>
 
           <b-form-group label="鐵蟲絲技能:" label-for="sp-skills">
@@ -80,8 +86,8 @@ export default {
               id="sp-skills"
               v-model="weaponData.sp_skills"
               placeholder="請輸入鐵蟲絲技能"
-              required>
-            </b-form-textarea>
+              required
+            ></b-form-textarea>
           </b-form-group>
 
           <b-form-group label="其他事項:" label-for="other-note">
@@ -89,15 +95,12 @@ export default {
               id="other-note"
               v-model="weaponData.other_note"
               placeholder="請輸入其他事項"
-              required>
-            </b-form-textarea>
+              required
+            ></b-form-textarea>
           </b-form-group>
-
         </b-form>
       </b-col>
     </b-row>
-    <b-button @click="save">
-      Save
-    </b-button>
+    <b-button @click="save">Save</b-button>
   </b-container>
 </template>
